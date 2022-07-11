58c7b3be793bc2c51947882f61bff4ca6d8ea163
ashok-kollipara <93081449+ashok-kollipara@users.noreply.github.com>
2022-07-11 09:54:48 +0530

    laptop gpu broken

--- .config/nvim/lua/keymaps.lua
+++ .config/nvim/lua/keymaps.lua
@@ -1,4 +1,10 @@
+-- Toggle line numbers
 vim.keymap.set('n','<F9>', function()
 	vim.opt.relativenumber = not vim.opt.relativenumber:get()
+	vim.opt.number = not vim.opt.number:get()
 end
 )
+
+-- Decrease/Increase split window width
+vim.keymap.set('n','<Space>[', "<cmd>:vertical :resize -5<CR>")
+vim.keymap.set('n','<Space>]', "<cmd>:vertical :resize +5<CR>")
